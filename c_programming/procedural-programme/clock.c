7b4fdd2d456ab496890c93b33f605723612c4ef8
martinaobrien <G00376463@gmit.ie>
2021-11-30 06:46:27 +0000

    Update clock.c
    Clock for c programming - different languages but user experience is the same

--- c_programming/procedural-programme/clock.c
+++ c_programming/procedural-programme/clock.c
@@ -1,5 +1,5 @@
 #include <stdio.h> //differences between OOP and PP
-#include <window.h> //way of thinking about the programme - there is the data and the methods are sepereate
+#include <windows.h> //way of thinking about the programme - there is the data and the methods are sepereate
 
 struct Clock { //data types
     int hours;
@@ -30,31 +30,33 @@ void tick(struct Clock *c) //same as in python algorithm
     }
 }
 
-void printClock(structClock c) //exists outside the data method and the struct has to be passed into it
+void printClock(struct Clock c) //exists outside the data method and the struct has to be passed into it
 {
     printf("\n Clock");
     printf("\n%02d:%02d:%02d", c.hours, c.mins, c.secs);
 }
 
-void validate(struct Clock*c) //passed in as a point as I don't need to change anything to print it out // this is a built in feature of more modern languages but you have to explicity do it older languages like c
+void validate(struct Clock *c) //passed in as a point as I don't need to change anything to print it out // this is a built in feature of more modern languages but you have to explicity do it older languages like c
 {
-    if (c -> hours > 12|| c ->mins >60 || c -> secs >60)
-    (
+    if (c->hours > 12 || c->mins > 60 || c->secs >60)
+    {
         c-> hours = 0;
         c-> mins = 0;
         c-> secs = 0;
-    )
+    }
 }
 
 int main ()
 {
-    struct Clock c = (10,12,24): //may need to change the data that is stored inside the struct and for that to happen I need to pass it in as a pointer so I can manipulate what is stored inside it
+    struct Clock c = {10,12,24}; //may need to change the data that is stored inside the struct and for that to happen I need to pass it in as a pointer so I can manipulate what is stored inside it
     validate(&c); //this idea is called pass by reference
 
-    while ()
+    while (1)
     {
-        tick(&c);
+        tick(&c); //passed by reference with &
         printClock(c);
-
+        Sleep(1000); //milliseconds
     }
-}
+    return 0;
+} //it is importnat to know who is responsible to what
+//from a user perspective it is the same
